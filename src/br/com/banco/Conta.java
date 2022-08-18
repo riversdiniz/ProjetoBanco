b3fbc08677b36c056db161b3688b0b69c9199d26
River Diniz <river-s-diniz@hotmail.com>
2022-08-18 11:52:40 -0300

    Colocado os métodos e construtores.

--- src/br/com/banco/Conta.java
+++ src/br/com/banco/Conta.java
@@ -2,8 +2,24 @@ package br.com.banco;
 
 public class Conta {
     
-    int numero;
-    double saldo;
+    private int numero;
+    private double saldo;
+
+    public double recuperarSaldo(){
+        return saldo;
+    }
+
+    public void depositar(double valor){
+        saldo = saldo + valor;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero(){
+        return numero;
+    }
 
     Cliente cliente = new Cliente();
 }
